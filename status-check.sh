b1a338a4fe1ef0b08ba6a4fe6c00a8c0f17f5f01
MOHAMMAD SUMAIR <91409244+sumair-aslam09@users.noreply.github.com>
2023-06-09 19:21:20 +0530

    Update status-check.sh

--- status-check.sh
+++ status-check.sh
@@ -4,9 +4,8 @@ echo "Testing the status of the Application"
 echo -n "..."
 sleep 120
 
-status=(curl -s -o /dev/null -w "%{http_code}"  "http://localhost:8081")
-if [ $status == 200 ];
-then
+status=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8081")
+if [ "$status" == "200" ]; then
   echo "Application is up and running"
 else
   echo "Application is not running"
