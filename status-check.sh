4bbb705e993dc3ae4e77ab6ffd151570708f0dcc
MOHAMMAD SUMAIR <91409244+sumair-aslam09@users.noreply.github.com>
2023-06-14 15:21:08 +0530

    Update status-check.sh

--- status-check.sh
+++ status-check.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# This script tests the status of the Application by sending a request to http://localhost:8081.
+# It waits for 2 minutes before checking the status.
 set -x
 echo "Testing the status of the Application"
 echo -n "..."
 sleep 120
 
 status=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:8081")
+# Check if the status code is 200 (indicating a successful response)
 if [ "$status" == "200" ]; then
   echo "Application is up and running"
 else
